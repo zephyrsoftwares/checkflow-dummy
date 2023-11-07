fb1341d7234d2dd3548d00300df15884619aa2c8
Ibrahim ADLANI <ibrahim@adlani.com>
2023-11-07 22:59:41 +0100

    Installed packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/images/logo.svg";
+import "./assets/styles/App.css";
+import React from "react";
 
 function App() {
   return (
